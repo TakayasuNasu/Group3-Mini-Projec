639683f84a16a9e813b4a4353ec65bdd5c24792e
heymiriam <miriambellon@gmail.com>
2020-11-10 16:36:16 -0800

    Improve the board

--- src/main/java/chess/Symbol.java
+++ src/main/java/chess/Symbol.java
@@ -2,19 +2,19 @@ package chess;
 
 public class Symbol {
 
-  public static final String EMPTY = "・";
+  public static final String EMPTY = " · ";
 
-  public static final String BLACK_KING = "\u2654";
-  public static final String BLACK_QUEEN = "\u2655";
-  public static final String BLACK_ROOK = "\u2656";
-  public static final String BLACK_BISHOP = "\u2657";
-  public static final String BLACK_KNIGHT = "\u2658";
-  public static final String BLACK_PAWN = "\u2659";
+  public static final String BLACK_KING = " ♚ ";
+  public static final String BLACK_QUEEN = " ♛ ";
+  public static final String BLACK_ROOK = " ♜ ";
+  public static final String BLACK_BISHOP = " ♝ ";
+  public static final String BLACK_KNIGHT = " ♞ ";
+  public static final String BLACK_PAWN = " ♟ ";
 
-  public static final String WHITE_KING = "\u265A";
-  public static final String WHITE_QUEEN = "\u265B";
-  public static final String WHITE_ROOK = "\u265C";
-  public static final String WHITE_BISHOP = "\u265D";
-  public static final String WHITE_KNIGHT = "\u265E";
-  public static final String WHITE_PAWN = "\u265F";
+  public static final String WHITE_KING = " ♔ ";
+  public static final String WHITE_QUEEN = " ♕ ";
+  public static final String WHITE_ROOK = " ♖ ";
+  public static final String WHITE_BISHOP = " ♗ ";
+  public static final String WHITE_KNIGHT = " ♘ ";
+  public static final String WHITE_PAWN = " ♙ ";
 }
